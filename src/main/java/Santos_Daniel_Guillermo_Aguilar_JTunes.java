6879a4c080b3ac6a55e58e02d89a8d8a30ab8f2c
tusderechos <hidrosoluciones61@gmail.com>
2025-06-20 13:57:13 -0600

    commit inicial

---
+++ src/main/java/Santos_Daniel_Guillermo_Aguilar_JTunes.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author Hp
+ */
+public class Santos_Daniel_Guillermo_Aguilar_JTunes {
+
+    public static void main(String[] args) {
+        System.out.println("gwngnwu");
+    }
+}
